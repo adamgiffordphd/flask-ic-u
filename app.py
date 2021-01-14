72f97a5ed8ae3f00ffc145de923cde4b76471dab
Adam Gifford <48251873+adamgiffordphd@users.noreply.github.com>
2021-01-14 17:21:47 -0500

    basic app outline created

--- app.py
+++ app.py
@@ -3,12 +3,20 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def intro():
+  return render_template('intro.html')
+
+@app.route('/howto')
+def howto():
+  return render_template('howto.html')
 
 @app.route('/about')
 def about():
   return render_template('about.html')
 
+@app.route('/forms')
+def forms():
+  return render_template('forms.html')
+
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run(port=33507, debug=True)
