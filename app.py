cf3ab9962988f33d4c665a70d0bbcef13566970f
Adam Gifford <adamgifford@adams-air.attlocal.net>
2021-01-29 21:51:00 -0500

    completed forms html and set up data input

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.resources import CDN
 import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
+app.vars = {}
+
+# load model
+
+def make_prediction(x_test):
+  # get prediction from model
+  y_pred = []
+  return make_plots(y_pred)
+
+def make_plots(y_pred):
+  # make plots
 
 @app.route('/')
 def intro():
@@ -22,9 +34,34 @@ def about():
 def forms():
   return render_template('forms.html')
 
+@app.route('/data', methods=['POST'])
+def predict():
+  app.vars['SUBJECT_ID'] = request.form.get('patientid')
+  app.vars['ADMIT_AGE'] = request.form.get('age')
+  app.vars['GENDER'] = request.form.get('gender')
+  app.vars['RELIGION'] = request.form['religion']
+  app.vars['LANGUAGE'] = request.form.get('language')
+  app.vars['MARITAL_STATUS'] = request.form['maritalstatus']
+  app.vars['ETHNICITY'] = request.form.get('ethnicity')
+
+  app.vars['ADMISSION_TYPE'] = request.form.get('admissiontype')
+  app.vars['ADMISSION_LOCATION'] = request.form.get('admissionlocation')
+  app.vars['INSURANCE'] = request.form['insurance']
+
+  app.vars['DIAGNOSIS'] = request.form.get('diagnosis')
+  app.vars['NOTES'] = request.form.get('clinicalnotes')
+
+  return redirect(url_for('display')) 
+
 @app.route('/display')
 def display():
-  n = 100
+  # x_test = pd.from_dict(app.vars)
+
+  # y_pred = make_prediction(x_test)
+  
+  # plots = make_plots(y_pred)
+
+  # n = 100
 
   x = np.random.random(n) * 10
   y = np.random.random(n) * 10
